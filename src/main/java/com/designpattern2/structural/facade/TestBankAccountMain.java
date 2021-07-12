c1d3cff661b33979218f747e7aa166b995eba4a1
Mugeesh Husain <mugeesh.husain@fleetship.com>
2021-07-12 17:13:47 +0800

    add definition

--- src/main/java/com/designpattern2/structural/facade/TestBankAccountMain.java
+++ src/main/java/com/designpattern2/structural/facade/TestBankAccountMain.java
@@ -2,14 +2,20 @@ package com.designpattern2.structural.facade;
 
 //reference : https://www.youtube.com/watch?v=B1Y8fcYrz5o&list=PLF206E906175C7E07&index=14
 
-public class TestBankAccount {
+/*
+ * Same goes for the Facade Design Pattern. 
+ * It hides the complexities of the system and provides an interface to the client 
+ * from where the client can access the system.
+ */
+
+public class TestBankAccountMain {
 	public static void main(String[] args) {
 		BankAccountFacade accessingBank = new BankAccountFacade(123456789, 1234);
 
 		accessingBank.withDrawCash(50.0);
 
 		accessingBank.withDrawCash(900.0);
-		
+
 		accessingBank.depositeCash(200.0);
 	}
 }
