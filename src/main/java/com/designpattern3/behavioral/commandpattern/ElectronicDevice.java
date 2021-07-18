52d449ce43b37abb7d5052a50f4970fb5386ef5a
mugeesh.husain <mugeesh@gmail.com>
2021-07-18 18:07:30 +0800

    added command design pattern

---
+++ src/main/java/com/designpattern3/behavioral/commandpattern/ElectronicDevice.java
@@ -0,0 +1,13 @@
+package com.designpattern3.behavioral.commandpattern;
+
+public interface ElectronicDevice {
+
+	public void on();
+
+	public void off();
+
+	public void volumeUp();
+
+	public void volumenDown();
+
+}
